0af3ffc371a9e14d69d99d0d75abafadc06f4e00
Phạm Nguyễn Phú Thành <thanhphamnguyenphu2k5@gmail.com>
2024-11-25 14:23:44 +0700

    complete responsive index

--- assets/js/responsive_navbar.js
+++ assets/js/responsive_navbar.js
@@ -23,3 +23,33 @@ exit.addEventListener("click", (e) => {
     re_navbar.classList.remove("re-navbar-active");
   }
 });
+
+const re_login = document.querySelector(".re-login");
+const re_login__desc = document.querySelector(".re-login__desc");
+const re_person = document.querySelector(".re-person");
+const re_mycart = document.querySelector(".re-mycart");
+const re_logout = document.querySelector(".re-logout");
+// Tự động đăng nhập
+window.addEventListener("load", (e) => {
+  e.preventDefault();
+  const loginUser = JSON.parse(localStorage.getItem("LoginUser"));
+  if (loginUser) {
+    re_login.href = "#!";
+    re_login__desc.textContent = loginUser.name;
+    re_person.classList.add("re-person-active");
+    re_mycart.classList.add("re-mycart-active");
+    re_logout.classList.add("re-logout-active");
+  }
+});
+
+// Tự động đăng xuất
+re_logout.addEventListener("click", (e) => {
+  e.preventDefault();
+  localStorage.removeItem("LoginUser");
+  re_login.href = "./login.html";
+  re_login__desc.textContent = "Đăng nhập";
+  re_person.classList.remove("re-person-active");
+  re_mycart.classList.remove("re-mycart-active");
+  re_logout.classList.remove("re-logout-active");
+  window.location.href = "./login.html";
+});
